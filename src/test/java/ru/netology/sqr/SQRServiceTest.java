27dddb420a91ff82e52b0e6745b04d3ada0bcb4c
Светлана Чернова <lana_19.87@mail.ru>
2022-03-28 20:23:19 +0600

    FixedParameterizedTest

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,16 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"sgrCounter1 ,200, 300, 3"})
+    @CsvSource(value = {"sgrCounter1 ,200, 300, 3", "sgrCounter2, 800, 2000, 16"})
     void sqrCounterParam(String testName, int randeFrom, int rangeUpTo, int expected) {
-        SQRService service = new SQRService();
-        int actual = service.sqrCounter(randeFrom, rangeUpTo);
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"sgrCounter1 ,800, 2000, 16"})
-    void sqrCounterParam1(String testName, int randeFrom, int rangeUpTo, int expected) {
         SQRService service = new SQRService();
         int actual = service.sqrCounter(randeFrom, rangeUpTo);
         assertEquals(expected, actual);
